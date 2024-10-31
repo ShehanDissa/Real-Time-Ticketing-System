b2ccc394e88df299fd04f7b1a64964d48faf6f9a
shehan.d <shehan.d@informaticsint.com>
2024-10-31 14:49:11 +0530

    Initial commit

---
+++ Backend/real-time-ticketing-system-be/src/main/java/com/example/real_time_ticketing_system_be/RealTimeTicketingSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_time_ticketing_system_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeTicketingSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeTicketingSystemBeApplication.class, args);
+	}
+
+}
