2db759472161ef864de85945f6fac99dce7c1a3e
shehan.d <shehan.d@informaticsint.com>
2024-11-01 14:03:07 +0530

    Recreated the backend project

---
+++ Backend/RealTimeTicketingSystem/src/main/java/org/oop/realtimeticketingsystem/RealTimeTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.oop.realtimeticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeTicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeTicketingSystemApplication.class, args);
+    }
+
+}
---
+++ Backend/RealTimeTicketingSystem/src/test/java/org/oop/realtimeticketingsystem/RealTimeTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oop.realtimeticketingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeTicketingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
